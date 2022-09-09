49fd6a00a2af8ae5bc7df183b1c76e7ac10d0976
Vitor Subhi <vitorsubhi@gmail.com>
2022-09-09 19:33:11 -0300

    feat(OpenObjects and Iploader): Added client-side of iploader and openobjects

---
+++ resources/[Scripts]/alternative/client-side/openobjects.lua
@@ -0,0 +1,46 @@
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- OPENOBJECTS
+-----------------------------------------------------------------------------------------------------------------------------------------
+Citizen.CreateThread(function()
+	while true do
+		local timeDistance = 999
+		local ped = PlayerPedId()
+		if not IsPedInAnyVehicle(ped) then
+			local coords = GetEntityCoords(ped)
+
+			local distance = #(coords - vector3(254.01,225.21,101.87))
+			if distance <= 3.0 then
+				timeDistance = 1
+
+				if IsControlJustPressed(1,38) then
+					local handle,object = FindFirstObject()
+					local finished = false
+
+					repeat
+						local heading = GetEntityHeading(object)
+						local coordsObj = GetEntityCoords(object)
+						local distanceObjs = #(coordsObj - coords)
+
+						if distanceObjs < 3.0 and GetEntityModel(object) == 961976194 then
+							if heading > 150.0 then
+								SetEntityHeading(object,0.0)
+							else
+								SetEntityHeading(object,160.0)
+							end
+
+							FreezeEntityPosition(object,true)
+							finished = true
+						end
+
+						finished,object = FindNextObject(handle)
+					until not finished
+
+					EndFindObject(handle)
+				end
+			end
+		end
+
+		Citizen.Wait(timeDistance)
+	end
+end)
