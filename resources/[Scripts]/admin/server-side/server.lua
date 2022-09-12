4a1a53e5cca6f3b5de271af1dff96bebbaf98907
Vitor Subhi <vitorsubhi@gmail.com>
2022-09-11 23:59:04 -0300

    feat(Debug): Added debug command to Admin

--- resources/[Scripts]/admin/server-side/server.lua
+++ resources/[Scripts]/admin/server-side/server.lua
@@ -163,6 +163,33 @@ RegisterCommand("tpcds",function(source,args,rawCommand)
 	end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
+-- DEBUG
+-----------------------------------------------------------------------------------------------------------------------------------------
+RegisterCommand("debug",function(source,args,rawCommand)
+	local source = source
+	local user_id = vRP.getUserId(source)
+	if user_id then
+		if vRP.hasPermission(user_id,"Admin") then
+			TriggerClientEvent("ToggleDebug",source)
+		end
+	end
+end)
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- SKIN
+-----------------------------------------------------------------------------------------------------------------------------------------
+RegisterCommand("skin",function(source,args,rawCommand)
+    local user_id = vRP.getUserId(source)
+	if vRP.hasPermission(user_id,"Admin") then
+		local nplayer = vRP.getUserSource(parseInt(args[1]))
+		if nplayer then
+			TriggerClientEvent("skinmenu",nplayer,args[2])
+			TriggerClientEvent("Notify",source,"amarelo","Setada a skin <b>"..args[2].."</b> no passaporte <b>"..parseInt(args[1]).."</b>.",5000)
+		end
+    end
+end)
+
+-----------------------------------------------------------------------------------------------------------------------------------------
 -- CDS
 -----------------------------------------------------------------------------------------------------------------------------------------
 RegisterCommand("cds",function(source,args,rawCommand)
